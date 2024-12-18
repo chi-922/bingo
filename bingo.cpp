e2be8b550800e407980dc2fb420784beb3616d1f
chi_927 <chi_927@naver.com>
2024-12-19 00:18:17 +0900

    사용자로부터 빙고 판 입력 받기

--- bingo.cpp
+++ bingo.cpp
@@ -1,10 +1,12 @@
 ﻿// bingo.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
 
+#define _CRT_NONSTDC_NO_WARNINGS    // 비표준 함수를 사용했을 때 에러 방지
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <Windows.h>   // 콘솔 형태 맞추기
+#include <conio.h>   // Visual Studio에서만 사용할 수 있고 리눅스나 OS X에서는 사용할 수 없습니다.
+
 using namespace std;
 
 void SetConsole() {
@@ -27,8 +29,91 @@ void SetConsole() {
     SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
 }
 
+
+void startScreen() {
+    cout << "\n\n\n\n";
+    cout << "\t\t"; cout << "@@@@@@@@@@@    @@@@@@@@@@@@    @@@@         @@      @@@@@@@@        @@@@@@@@    \n";
+    cout << "\t\t"; cout << "@@        @@        @@         @@ @@        @@    @@              @@        @@  \n";
+    cout << "\t\t"; cout << "@@        @@        @@         @@   @@      @@   @@              @@          @@ \n";
+    cout << "\t\t"; cout << "@@@@@@@@@@@         @@         @@     @@    @@   @@              @@          @@ \n";
+    cout << "\t\t"; cout << "@@        @@        @@         @@       @@  @@   @@     @@@@@    @@          @@ \n";
+    cout << "\t\t"; cout << "@@        @@        @@         @@        @@ @@    @@        @@    @@        @@  \n";
+    cout << "\t\t"; cout << "@@@@@@@@@@@    @@@@@@@@@@@@    @@         @@@@     @@@@@@@@@@       @@@@@@@@ \n\n\n\n\n";
+    cout << "\t\t\t"; cout << "              게임을 시작하려면 아무키나 누르세요.\n\n\n\n\n\n\n";
+
+    getch(); // 아무키 입력 기다림
+    system("cls"); // 콘솔 창 clear
+}
+
+
+// 사용자가 빙고에 숫자 집어넣을 때 중복 탐색
+bool isDuplicate(const vector<vector<int>>* userBingo, int* input) {
+    for (const auto& row : *userBingo) {
+        for (const auto& num : row) {
+            if (num == *input) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
 int main() {
     SetConsole();
+
+    // 시작화면
+    startScreen();
+
+    // 본 게임 5 * 5
+    const int SIZE = 5;
+    vector<vector<int>> userBingo(SIZE, vector<int>(SIZE, 0)); // 2차원 벡터 초기화;
+    int input; // 사용자의 입력값
+    int count = 0; // 현재까지 입력된 숫자의 개수
+
+    while (count < SIZE * SIZE) {
+        // 잘 들어왔는지 확인용
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << "----------------" << endl;
+            for (int j = 0; j < SIZE; j++)
+            {
+                cout << "|" << userBingo[i][j];
+                if (j == SIZE - 1) {
+                    cout << "|" << endl;
+                }
+            }
+            if (i == SIZE - 1) {
+                cout << "----------------" << endl;
+            }
+        }
+        
+        cout << "\n\n\n";
+
+        // userBingo에 수 넣기
+        cout << "빙고" << count + 1 << "번째 칸 채우는 중  :  ";
+        cin >> input;
+
+        if (input < 1 || input > 30) {
+            cout << "숫자 범위를 벗어났습니다. 다시 입력해주세요.";
+            Sleep(1000);   // windows에 api 를 이용
+            system("cls");
+            continue;
+        }
+        else if (isDuplicate(&userBingo, &input)) {
+            cout << "이미 입력된 숫자입니다. 다른 숫자를 입력해주세요." << endl;
+            Sleep(1000);
+            system("cls");
+            continue;
+
+        }
+
+         // 입력값을 2차원 벡터에 저장
+        userBingo[count / SIZE][count % SIZE] = input;
+        count++; // 입력 개수 증가
+        system("cls");
+    }
+
     cout << "Hello, World! This is a customized console window.\n";
     return 0;
 }
